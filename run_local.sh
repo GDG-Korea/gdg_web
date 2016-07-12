f25d88950c33f28d648bb108aa4dea92308dcf88
nurinamu <nurinamu@gmail.com>
2016-07-12 13:41:35 +0900

    tomcat 환경 추가.

--- run_local.sh
+++ run_local.sh
@@ -3,4 +3,4 @@ HERE=$(pwd)
 echo "[$HERE] will be connected to server root."
 docker build docker -t gdg:local
 echo "Run GDG container!"
-docker run -d -p 80:80 -v $HERE/site:/home/gdg gdg:local
+docker run -d -p 80:80 -p 8080:8080 -v $HERE/site:/home/gdg gdg:local
